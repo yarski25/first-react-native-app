a1b62c2447a34484928c63bdde84ac3e19ef6f80
yarski25 <yaroslav.saprykin@gmail.com>
2024-09-09 18:00:36 +0200

    getLocation implemented

--- App.tsx
+++ App.tsx
@@ -23,33 +23,35 @@ import Section from 'components/Section/Section';
 import Header from 'components/Header/Header';
 import Back from 'components/Icons/Back/Back';
 import Avatar from 'assets/icons/svg/avatar.svg';
-// import Geolocation from '@react-native-community/geolocation';
+import Geolocation, {
+  GeolocationResponse,
+} from '@react-native-community/geolocation';
 
 // Function to get permission for location
-// const requestLocationPermission = async () => {
-//   try {
-//     const granted = await PermissionsAndroid.request(
-//       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-//       {
-//         title: 'Geolocation Permission',
-//         message: 'Can we access your location?',
-//         buttonNeutral: 'Ask Me Later',
-//         buttonNegative: 'Cancel',
-//         buttonPositive: 'OK',
-//       },
-//     );
-//     console.log('granted', granted);
-//     if (granted === 'granted') {
-//       console.log('You can use Geolocation');
-//       return true;
-//     } else {
-//       console.log('You cannot use Geolocation');
-//       return false;
-//     }
-//   } catch (err) {
-//     return false;
-//   }
-// };
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Geolocation Permission',
+        message: 'Can we access your location?',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    console.log('granted', granted);
+    if (granted === 'granted') {
+      console.log('You can use Geolocation');
+      return true;
+    } else {
+      console.log('You cannot use Geolocation');
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
 
 const App = (): React.JSX.Element => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -59,31 +61,32 @@ const App = (): React.JSX.Element => {
   };
 
   // state to hold location
-  // const [location, setLocation] = useState(false);
+  const [location, setLocation] = useState<GeolocationResponse | null>(null);
 
   // Geolocation.setRNConfiguration(config);
 
   // function to check permissions and get Location
-  // const getLocation = () => {
-  //   const result = requestLocationPermission();
-  //   result.then(res => {
-  //     console.log('res is:', res);
-  //     if (res) {
-  //       Geolocation.getCurrentPosition(
-  //         position => {
-  //           console.log(position);
-  //         },
-  //         error => {
-  //           // See error code charts below.
-  //           console.log(error.code, error.message);
-  //           setLocation(false);
-  //         },
-  //         {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-  //       );
-  //     }
-  //   });
-  //   console.log(location);
-  // };
+  const getLocation = () => {
+    const result = requestLocationPermission();
+    result.then(res => {
+      console.log('res is:', res);
+      if (res) {
+        Geolocation.getCurrentPosition(
+          position => {
+            console.log(position);
+            setLocation(position);
+          },
+          error => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+            setLocation(null);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+    });
+    console.log(location);
+  };
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -112,12 +115,22 @@ const App = (): React.JSX.Element => {
               style={{
                 borderRadius: 10,
               }}>
-              <Button title="Get Location" />
+              <Button title="Get Location" onPress={getLocation} />
             </View>
             {'\n\n'}
-            <Text>Latitude: </Text>
+            <Text>
+              Latitude:{' '}
+              {location?.coords.latitude
+                ? location.coords.latitude.toFixed(3)
+                : null}
+            </Text>
             {'\n'}
-            <Text>Longitude: </Text>
+            <Text>
+              Longitude:{' '}
+              {location?.coords.longitude
+                ? location.coords.longitude.toFixed(3)
+                : null}{' '}
+            </Text>
             <View
               style={{
                 marginTop: 10,
